4289bb1d8203845a11a49f5cdb8239173d53b90f
yytay <yytay@hotmail.com>
2020-08-08 22:27:00 +0800

    Update Configuration_adv.h
    test on termal runaway

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -182,8 +182,8 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-  #define WATCH_TEMP_PERIOD 20                // Seconds
-  #define WATCH_TEMP_INCREASE 2               // Degrees Celsius
+  #define WATCH_TEMP_PERIOD 300                // Seconds
+  #define WATCH_TEMP_INCREASE 8               // Degrees Celsius
 #endif
 
 /**
